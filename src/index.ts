beea92be142e74105ab43d0ffdb72d519439ac0b
Robot-Inventor <30.keita@gmail.com>
2024-11-13 01:33:10 +0900

    feat: add `isPlainObject()` function

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Check if the value is a plain object.
+ *
+ * @example
+ * isPlainObject({}) // true
+ * isPlainObject([]) // false
+ * isPlainObject(null) // false
+ *
+ * @param value The value to check.
+ * @returns Returns `true` if the value is a plain object, otherwise `false`.
+ */
+const isPlainObject = (value: unknown): value is Record<string, unknown> =>
+    typeof value === "object" && value !== null && !Array.isArray(value);
+
+export { isPlainObject };

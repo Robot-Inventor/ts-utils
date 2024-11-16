5b8e7899204a94a838634f0660e630bef4435d6a
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2024-11-16 22:16:44 +0900

    feat: add an optional `protocol` argument to `isValidUrl()` function (#14)
    * feat: add an optional `protocol` argument to `isValidUrl()` function
    
    * Create odd-goats-dream.md

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,22 @@ const isPlainObject = (value: unknown): value is Record<string, unknown> =>
 /**
  * Check if the value is a valid URL.
  * @param value The value to check.
- * @returns Returns a URL object if the value is a valid URL, otherwise `null`.
+ * @param protocol Optional protocol(s) to check if the URL starts with.
+ * @returns Returns a URL object if the value is a valid URL and matches the protocol(s), otherwise `null`.
  */
-const isValidUrl = (value: string): null | URL => {
+const isValidUrl = (value: string, protocol?: string | string[]): null | URL => {
     try {
-        return new URL(value);
+        const url = new URL(value);
+
+        if (protocol) {
+            const protocols = Array.isArray(protocol) ? protocol : [protocol];
+            // eslint-disable-next-line no-magic-numbers
+            if (!protocols.includes(url.protocol.slice(0, -1))) {
+                return null;
+            }
+        }
+
+        return url;
     } catch {
         return null;
     }

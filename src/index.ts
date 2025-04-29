61666d85bffcb3395b1fce250b3ee0501032b555
Robot-Inventor <30.keita@gmail.com>
2025-04-29 11:44:07 +0900

    chore: remove deprecated `isValidUrl()` function

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,6 @@ const isPlainObject = (value: unknown): value is Record<string, unknown> =>
     typeof value === "object" && value !== null && !Array.isArray(value);
 
 /**
- * Check if the value is a valid URL.
- * @deprecated Use [`URL.parse()`](https://developer.mozilla.org/docs/Web/API/URL/parse_static) instead.
- * @param value The value to check.
- * @param protocol Optional protocol(s) to check if the URL starts with.
- * @returns Returns a URL object if the value is a valid URL and matches the protocol(s), otherwise `null`.
- */
-const isValidUrl = (value: string, protocol?: string | string[]): null | URL => {
-    try {
-        const url = new URL(value);
-
-        if (protocol) {
-            let protocols = Array.isArray(protocol) ? protocol : [protocol];
-            // eslint-disable-next-line no-magic-numbers
-            protocols = protocols.map((prt) => (prt.endsWith(":") ? prt.slice(0, -1) : prt));
-            // eslint-disable-next-line no-magic-numbers
-            if (!protocols.includes(url.protocol.slice(0, -1))) {
-                return null;
-            }
-        }
-
-        return url;
-    } catch {
-        return null;
-    }
-};
-
-/**
  * Represents an array with at least one element.
  * @param T - The type of the elements in the array.
  */
@@ -66,4 +39,4 @@ type AssertType<T extends Expected, Expected> = T;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-unnecessary-type-parameters
 const assertType = <Expected>(_target: Expected): true => true;
 
-export { isPlainObject, isValidUrl, type NonEmptyArray, isNonEmptyArray, type AssertType, assertType };
+export { isPlainObject, type NonEmptyArray, isNonEmptyArray, type AssertType, assertType };
